3099c6ff1ffe5e9f81133e5c25f06f0827ab0ee7
Priyankas1507 <140598699+Priyankas1507@users.noreply.github.com>
2024-11-26 16:05:14 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    # Build your project
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u priyankavijay -p dckr_pat_98Dx8iTWLuG96HX1jzNU1eSgMz0
+    docker tag test priyankavijay/dev
+    docker push priyankavijay/dev
+
+elif [[ $GIT_BRANCH == "origin/main" ]]; then
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u priyankavijay -p dckr_pat_98Dx8iTWLuG96HX1jzNU1eSgMz0
+    docker tag test priyankavijay/prod 
+    docker push priyankavijay/prod
+fi
