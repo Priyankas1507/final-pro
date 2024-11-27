cb94a5ea4de4bd10a0c52a269f82d423bb6f2e04
Priyankas1507 <140598699+Priyankas1507@users.noreply.github.com>
2024-11-27 15:56:10 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $GIT_BRANCH = "origin/dev" ]; then
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
@@ -7,7 +7,7 @@ if [ $GIT_BRANCH = "origin/dev" ]; then
     docker tag test priyankavijay/dev
     docker push priyankavijay/dev
 
-elif [ $GIT_BRANCH = "origin/master" ]; then
+elif [[ $GIT_BRANCH == "master" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u priyankavijay -p dckr_pat_98Dx8iTWLuG96HX1jzNU1eSgMz0
